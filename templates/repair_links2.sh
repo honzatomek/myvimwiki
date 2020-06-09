be7db4782da3f1174b2f9f9563d000b6c7c8a241
honzatomek <rpi3.tomek@protonmail.com>
2020-06-10 00:01:20 +0200

    one line version of script to repair relative HTML links using awk

---
+++ templates/repair_links2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for f in ~/vimwiki/vimwiki_html/*.html; do
+#   cat "${f}" | awk 'match($0, /(^.*<a href=\")(#.*)(\">.*$)/, arr) { gsub(/ /, "%20", arr[2]); gsub(/\.html$/, "", arr[2]); gsub(/\.md/, "", arr[2]); print arr[1] arr[2] arr[3] }'
+  cat "${f}" | awk '{ match($0, /(^.*<a href=\")(#.*)(\">.*$)/, arr); if (arr[2] == "") print $0; else { gsub(/ /, "%20", arr[2]); gsub(/\.html$/, "", arr[2]); gsub(/\.md/, "", arr[2]); print arr[1] arr[2] arr[3] }; }' > "${f}"
+done
+
