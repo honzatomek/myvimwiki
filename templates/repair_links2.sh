9bd8fdee386062e7cfcac0ea88865e5978aa77b8
honzatomek <rpi3.tomek@protonmail.com>
2020-06-10 00:57:02 +0200

    removed cat - was redundant

--- templates/repair_links2.sh
+++ templates/repair_links2.sh
@@ -2,7 +2,7 @@
 
 for f in ~/vimwiki/vimwiki_html/*.html; do
 #   cat "${f}" | awk 'match($0, /(^.*<a href=\")(#.*)(\">.*$)/, arr) { gsub(/ /, "%20", arr[2]); gsub(/\.html$/, "", arr[2]); gsub(/\.md/, "", arr[2]); print arr[1] arr[2] arr[3] }'
-  cat "${f}" | awk '
+  awk '
   {
     match($0, /(^.*<a href=\")(#.*)(\">.*$)/, arr);
     if (arr[2] == "")
@@ -13,6 +13,6 @@ for f in ~/vimwiki/vimwiki_html/*.html; do
         gsub(/\.html$/, "", arr[2]);
         gsub(/\.md/, "", arr[2]);
         print arr[1] arr[2] arr[3] };
-      }' # > "${f}"
+      }' "${f}" # > "${f}"
 done
 
